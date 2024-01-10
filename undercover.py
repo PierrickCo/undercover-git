e9aa80355c54e04f090c1e8198b89f12a3a6b493
Undercover Dev <dev@undercover.game>
2024-01-10 10:00:00 +0000

    v2.0 - Version Web mobile
    - Migration Python -> HTML/CSS/JS
    - Interface tactile optimisée iPhone
    - Vote par tap sur le nom du joueur
    - Système de points inter-manches
    - Podium avec classement visuel
    - Fonctionne hors-ligne

--- undercover.py
+++
@@ -1,4 +0,0 @@
-# Version 1.0 - Python terminal (voir historique complet dans undercover.html)
-# Jeu Undercover en ligne de commande avec couleurs ANSI
-print("Undercover v1.0 - Python CLI")
-# Fix TERM environment, gestion EOF
